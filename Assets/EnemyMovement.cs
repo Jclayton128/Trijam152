29fef0101f392db1cb9277657242aad5bc5bb538
jclayton128 <jclayton128@gmail.com>
2022-01-14 10:19:36 -0500

    Enemy

---
+++ Assets/EnemyMovement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    Rigidbody2D rb;
+
+    //param
+    public float moveForce = 1f;
+
+    //state
+    Vector2 moveInput = Vector2.zero;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.DrawLine(transform.position, transform.position + (Vector3)rb.velocity, Color.green);
+    }   
+
+    private void FixedUpdate()
+    {
+        rb.AddForce(moveInput * moveForce, ForceMode2D.Impulse);
+
+    }
+}
